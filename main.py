a78cd2429bb63255c33e4d3cce40bc0c2f5b5573
scorpion-00 <chetanikagnihotri@gmail.com>
2025-05-15 19:28:00 +0530

    Reorder static file mounting in main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import asyncio
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/hello")
 async def say_hello():
     await asyncio.sleep(5)
     return {"message": "hello world"}
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
 
 if __name__ == "__main__":
     import uvicorn
