7bce61b34a8d173c03b76af53c716b6b08719a0f
scorpion-00 <chetanikagnihotri@gmail.com>
2025-05-15 16:46:12 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import asyncio
+
+app = FastAPI()
+
+@app.get("/hello")
+async def say_hello():
+    await asyncio.sleep(5)
+    return {"message": "hello world"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
